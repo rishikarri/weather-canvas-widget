6b9b9b7c50a1fb900b1c17bd188d471cb3e75a8f
Rishi Karri <rishikarri1@gmail.com>
2016-12-20 14:21:10 -0500

    Commit 2

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-console.log("hi");
+$(document).ready(function(){
+	var weatherURL = "http://api.openweathermap.org/data/2.5/weather?zip=30324,us&appid=" + apiKey;
+
+	console.log(weatherURL);
+
+	$.getJSON(weatherURL, function(weatherData){
+		console.log(weatherData);
+	})
+})
